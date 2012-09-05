73594921380d8f41b84d1181fad7ecb6feae2902
Pierpaolo Pernici <pierpaolo@gmail.com>
2012-09-05 09:13:12 +0200

    Goo.im updater support
    Signed-off-by: Pierpaolo Pernici <pierpaolo@gmail.com>

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -1,16 +1,17 @@
 # Version information used on all builds
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=JRO03H BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
-DATE = $(shell date +%h-%d-%y)
+DATE = $(shell date +%Y%m%d)
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.aokp.version=$(TARGET_PRODUCT)_jb-build-1_$(DATE)
 
 # Goo updater app
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.goo.developerid=aokp \
-    ro.goo.rom=aokp \
-    ro.goo.version=21
+    ro.goo.developerid=RootBox \
+    ro.goo.board=GT-I9100 \
+    ro.goo.rom=AOKP_i9100 \
+    ro.goo.version=$(DATE)
 
 # Camera shutter sound property
 PRODUCT_PROPERTY_OVERRIDES += \
