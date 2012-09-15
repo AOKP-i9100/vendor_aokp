e72ea1b76d98cae1291ab2196b5a92534ce590ab
Pierpaolo Pernici <pierpaolo@gmail.com>
2012-09-15 09:13:20 +0200

    common_versions: build-2

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -1,10 +1,10 @@
 # Version information used on all builds
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=JRO03H BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=JRO03L BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
 DATE = $(shell date +%Y%m%d)
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.aokp.version=$(TARGET_PRODUCT)_jb-build-1_$(DATE)
+    ro.aokp.version=$(TARGET_PRODUCT)_jb-build-2_$(DATE)
 
 # Goo updater app
 PRODUCT_PROPERTY_OVERRIDES += \
